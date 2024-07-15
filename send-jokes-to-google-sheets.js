c3a3f082c7138c9bb72e4fd039b0c3b948492f76
Tom TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-07-15 11:30:05 +0200

    Update send-jokes-to-google-sheets.js

--- send-jokes-to-google-sheets.js
+++ send-jokes-to-google-sheets.js
@@ -8,7 +8,7 @@ async function fetchJoke() {
 
 // Fonction pour envoyer la blague au webhook Google Apps Script
 async function sendJokeToGoogleSheets(joke) {
-_    const webhookUrl = process.env.JOKE_WEBHOOK_URL; // Utilise le secret
+    const webhookUrl = process.env.JOKE_WEBHOOK_URL; // Utilise le secret
 
     await axios.post(webhookUrl, joke);
 }
